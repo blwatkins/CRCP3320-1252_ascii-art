256481b8ff128f5c87ff29e73aeac4dc9590f222
Jaisan Avery <jdavery@smu.edu>
2025-02-19 23:35:39 -0600

    added squirtle ascii art to the sh file and added the title to the valid art names list

--- ascii-art.sh
+++ ascii-art.sh
@@ -14,6 +14,43 @@ clover_art() {
     echo "jgs^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
 }
 
+# added by jaisannotjaxsxn
+squirtle_art() {
+    echo "             _,........__"
+    echo "          ,-'            \"\`-."
+    echo "        ,'                   \`-."
+    echo "      ,'                        \\"
+    echo "     ,'                          ."
+    echo "     .'\\               ,\"\".      \`"
+    echo "    ._.'|             / |  \`       \\"
+    echo "    |   |            \`-.'  ||       \`."
+    echo "    |   |            '-._,'||       | \\"
+    echo "    .\`.,'             \`..,'.'       , |\`-."
+    echo "    l                       .'\`.  _/  |   \`."
+    echo "    \`-.._'-   ,          _ _'   -\" \\  .     \`"
+    echo "\`.\"\"\"\"\"'-.\`-...,---------','         \`. \`....__."
+    echo ".'        \`\"-..___      __,'\\          \\  \\     \\"
+    echo "\\_ .          |   \`\"\"\"\"'    \`.           . \\     \\"
+    echo "  \`.          |              \`.          |  .     L"
+    echo "    \`.        |\`--...________.'.        j   |     |"
+    echo "      \`._    .'      |          \`.     .|   ,     |"
+    echo "         \`--,\\       .            \`7\"\"' |  ,      |"
+    echo "            \` \`      \`            /     |  |      |    _,-'\"\"\"\`-."
+    echo "             \\ \`.     .          /      |  '      |  ,'          \`."
+    echo "              \\  v.__  .        '       .   \\    /| /              \\"
+    echo "               \\/    \`\"\"\\\"\"\"\"\"\"\"\`.       \\   \\  /.''                |"
+    echo "                \`        .        \`._ ___,j.  \`/ .-       ,---.     |"
+    echo "                ,\`-.      \\         .\"     \`.  |/        j     \`    |"
+    echo "               /    \`.     \\       /         \\ /         |     /    j"
+    echo "              |       \`-.   7-.._ .          |\"          '         /"
+    echo "              |          \`./_    \`|          |            .     _,'"
+    echo "              \`.           / \`----|          |-............\`---'"
+    echo "                \\          \\      |          |"
+    echo "               ,'           )     \`.         |"
+    echo "                7____,,..--'      /          |"
+    echo "                                  \`---.__,--.'"
+}
+
 while getopts n: flag
 do
     case "${flag}" in
@@ -35,10 +72,14 @@ then
 elif [ "$artName" == "squiggle" ]
 then
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+elif [ "$artName" == "squirtle" ]   #added by jaisannotjaxsxn
+then
+    squirtle_art
 else
     echo "FAILURE: No such art name."
     echo "Valid art names:"
     echo "    clover"
     echo "    squiggle"
+    echo "    squirtle"
     exit 1
 fi
